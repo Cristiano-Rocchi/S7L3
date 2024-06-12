fc84f0f0ad93e59b42164cbca0f1c2ac60da7cc8
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-06-12 17:34:25 +0200

    punto 1 e punto 2

--- index.js
+++ index.js
@@ -6,8 +6,37 @@ const fetchBooks = () => {
         return responseObj.json();
       }
     })
-    .then((booksObj) => {
-      console.log("bokks Object", booksObj);
+    .then((books) => {
+      console.log("books", books);
+      const row = document.getElementById("cardsConteiner");
+      books.forEach((element) => {
+        const col = document.createElement("div");
+        col.classList.add("col");
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const img = document.createElement("img");
+        img.src = element.img;
+
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("cardBody");
+
+        const cardTitle = document.createElement("h3");
+        cardTitle.classList.add("cardTitle");
+        cardTitle.textContent = element.title;
+        const cardPrice = document.createElement("p");
+        cardPrice.textContent = element.price;
+
+        /* col.appendChild(card);
+        cardsConteiner.appendChild(col); */
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardPrice);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        col.appendChild(card);
+        row.appendChild(col);
+      });
     })
     .catch((err) => console.log(err));
 };
